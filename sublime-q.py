34789122bf365970757f3f76d8fad57ce054ecd9
komsit <komsit@khroton.com>
2014-10-25 15:38:42 +0900

    show results in output view

--- sublime-q.py
+++ sublime-q.py
@@ -2,6 +2,7 @@ import sublime
 import sublime_plugin
 import string
 from qpython import qconnection
+from qpython.qtype import QException
 
 class QSendCommand(sublime_plugin.TextCommand):
 
@@ -13,13 +14,20 @@ class QSendCommand(sublime_plugin.TextCommand):
 
         # Split s into lines
         s = s.encode('ascii')  # qpy needs ascii
-        print(s)
+        print s
         #q = qpy.conn(host=host, port=int(port))
         q = qconnection.QConnection(host = host, port = int(port))
         q.open()
-        res = q('.Q.s ' + s)
-        print(res)
-        q.close()
+        try:
+            res = q('.Q.s ' + s)
+        except QException, msg:
+            res = " `" + str(msg)
+        finally:
+            print "close"
+            q.close()
+
+        return res
+        
 
     def run(self, edit):
         global settings
@@ -42,7 +50,11 @@ class QSendCommand(sublime_plugin.TextCommand):
         if(s == "" or s == "\n"):
             return
 
-        self.send(s)
+        res = self.send(s)
+        print(res)
+        self.show_tests_panel()
+        self.append_data(res)
+        
 
     def advanceCursor(self, region):
         (row, col) = self.view.rowcol(region.begin())
@@ -57,6 +69,30 @@ class QSendCommand(sublime_plugin.TextCommand):
         # Remove the old region and add the new one
         self.view.sel().subtract(region)
         self.view.sel().add(sublime.Region(loc, loc))
+
+    def window(self):
+        return self.view.window()
+
+    def append_data(self, data):
+        self.output_view.set_read_only(False)
+        edit = self.output_view.begin_edit()
+        self.output_view.insert(edit, self.output_view.size(), data)
+        self.output_view.end_edit(edit)
+        self.output_view.set_read_only(True)
+
+    def show_tests_panel(self):
+        if not hasattr(self, 'output_view'):
+            self.output_view = self.window().get_output_panel("tests")
+        self.output_view.set_syntax_file("Packages/Ocaml/OCamlyacc.tmLanguage")
+        self.clear_test_view()
+        self.window().run_command("show_panel", {"panel": "output.tests"})
+
+    def clear_test_view(self):
+        self.output_view.set_read_only(False)
+        edit = self.output_view.begin_edit()
+        self.output_view.erase(edit, sublime.Region(0, self.output_view.size()))
+        self.output_view.end_edit(edit)
+        self.output_view.set_read_only(True)
 
 
 class QConnectCommand(sublime_plugin.WindowCommand):
